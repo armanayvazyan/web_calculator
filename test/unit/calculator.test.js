292fd9e514587dd835a3cd58617bdaf9a2c942e5
armanayvazyan <arman.ayvazyan@jetbrains.com>
2023-05-17 15:55:21 +0400

    jenkinsfile

--- test/unit/calculator.test.js
+++ test/unit/calculator.test.js
@@ -4,7 +4,7 @@ describe("Test calculator functionality", () => {
     it("Addition operation works as expected", async () => {
         const num1 = 10;
         const num2 = 13;
-        expect(calculator.add(num1, num2)).toBe(num1-num2);
+        expect(calculator.add(num1, num2)).toBe(num1+num2);
     });
 
     it("Multiple operation works as expected", async () => {

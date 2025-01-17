eda2d576fbb4c752320d3a844484cd0c97ac3e6c
armanayvazyan <arman.ayvazyan@jetbrains.com>
2025-01-17 12:51:05 +0400

    Update dependencies and add new packages
    Updated various @babel packages to their latest versions for improved compatibility and fixed issues. Added `cors` to dependencies and `nyc` to devDependencies in package-lock.json. These changes enhance functionality and maintain consistency across dependencies.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const api = require('./backend/api')
 const path = require('path');
+const cors = require('cors'); // Import the cors middleware
 const app = express()
 
+app.use(cors())
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname +'/frontend/calculator.html'));
 });
